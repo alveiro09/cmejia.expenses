dea41ea32584f2f5fe636fa73b97ede4db1a26c2
Cristian Alveiro Mejia Obando <alveiro09@hotmail.com>
2020-07-20 16:42:03 -0500

    [Back] add migration changeTypeOfIdentityDocument

--- Back/User.Domain.Infraestructure/Migrations/20200720213931_changeTypeOfIdentityDocument.Designer.cs
+++ Back/User.Domain.Infraestructure/Migrations/20200720213931_changeTypeOfIdentityDocument.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UserManagement.Domain.Infraestructure;
 
 namespace UserManagement.Domain.Infraestructure.Migrations
 {
     [DbContext(typeof(UserManagementContext))]
-    partial class UserManagementContextModelSnapshot : ModelSnapshot
+    [Migration("20200720213931_changeTypeOfIdentityDocument")]
+    partial class changeTypeOfIdentityDocument
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Back/User.Domain.Infraestructure/Migrations/20200720213931_changeTypeOfIdentityDocument.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UserManagement.Domain.Infraestructure.Migrations
+{
+    public partial class changeTypeOfIdentityDocument : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "identitynumber",
+                table: "users",
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "identitynumber",
+                table: "users",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+    }
+}
